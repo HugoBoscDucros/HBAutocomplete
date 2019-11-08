cae48d89df54e2d98309b5620a0f8b09a0f1f485
Hugo <hugo.boscducros@gmail.com>
2019-11-08 17:39:57 +0100

    added localizedStrings in test project

---
+++ AutocompleteTestProject/Ressources/wording.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  AutocompleteTestProject
+//
+//  Created by Hugo Bosc-Ducros on 08/11/2019.
+//  Copyright © 2019 Hugo Bosc-Ducros. All rights reserved.
+//
+
+import Foundation
+
+let BATMAN = NSLocalizedString("Batman", comment: "") as String
+let CATWOMAN = NSLocalizedString("Catwoman", comment: "") as String
+let FLASH = NSLocalizedString("Flash", comment: "") as String
+let SPIDERMAN = NSLocalizedString("Spiderman", comment: "") as String
+let SUPERMAN = NSLocalizedString("Superman", comment: "") as String
+let ZORO = NSLocalizedString("Zoro", comment: "") as String
